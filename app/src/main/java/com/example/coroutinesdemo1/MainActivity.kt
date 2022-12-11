280bb4e091751589e92061b4f4dc42d145ce9256
halil.olcay <holcay92@gmail.com>
2022-12-11 18:35:50 +0300

    CoRoutine added on the code and context switch is used

--- app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var messageTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,18 +17,28 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.tvCount)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
+        messageTextView = findViewById(R.id.tvMessage)
 
         countButton.setOnClickListener {
             textView.text = count++.toString()
         }
         downloadButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
                 downloadUserData()
+            }
         }
     }
-}
+    // we used suspend keyword to make this function delayable
+// sometimes logcat will not be able to catch all the logs and gives EOF error
+// to avoid this we use delay function
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                messageTextView.text = "Downloading user $i "
+            }
 
-private fun downloadUserData() {
-    for (i in 1..200000) {
-        Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            delay(100)
+        }
     }
 }
